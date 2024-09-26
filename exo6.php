6fcba24ce9d26fb1be514f4685d1e6fce496e467
riandomt <dorian.mittou@ikmail.com>
2024-09-26 11:50:09 +0200

    version terminé de la séance

--- exo6.php
+++ exo6.php
@@ -4,19 +4,19 @@ echo "Jeu du pendu : retrouvez le mot mystère !\n" ;
 $mot_a_trouver = "PROGRAMMATION" ;
 $mot_a_completer = "_____________" ;
 $essais_restants = 12 ;
-while($mot_a_trouver != $mot_a_completer){
+while($essais_restants > 0 || $mot_a_completer = $mot_a_trouver){
     echo "Mot à compléter : {$mot_a_completer}\n" ;
     echo "Nombre d'essais restants : {$essais_restants}\n" ;
     $lettre = readline("Indiquez votre lettre en majuscule, et validez avec la touche Entrée : ") ;
     $lettre_absente = true ;
-    for(????????){
+    for($i=0;$i<strlen($mot_a_trouver);$i){
         if($mot_a_trouver[$i]==$lettre){
-            ???????????
-			???????????
+            $mot_a_completer[$i] = $lettre;
+            $lettre_absente = false;
         }
     }
     if($lettre_absente){
-        $essais_restants -= ???? ;
+        $essais_restants -= 1;
     }
 }
 if($essais_restants>0){
@@ -25,3 +25,5 @@ if($essais_restants>0){
 else{
     echo "Pendu ! Le mot mystère était {$mot_a_trouver}\n" ;
 }
+
+?>
